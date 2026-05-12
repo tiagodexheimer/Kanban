d2d301acbd903142877d883281493a73d6b918be
Tiago Dexheimer <tiagodexheimer@gmail.com>
2026-05-12 11:02:15 -0300

    fix: auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -58,5 +58,5 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "fallback-secret-for-dev-only",
 };
